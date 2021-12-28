265e408b568e4f0bcdbafe95a1b0e57e13584379
Gerson Azabache <gerson_dunk@hotmail.com>
2021-12-28 16:47:59 -0500

    initial

---
+++ NetCoreCRUDAjax/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCoreCRUDAjax.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
